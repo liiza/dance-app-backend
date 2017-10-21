214f37b67633fb919697e328e1b6e0cda46301e3
liisa Sailaranta <liisa.sailaranta>
2017-10-20 21:33:51 -0700

    Add logging to help debugging.

--- backend/views.py
+++ backend/views.py
@@ -2,13 +2,17 @@ from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
+import logging
 import json
 
 from .models import Record
 
+logger = logging.getLogger(__name__)
+
 class SaveDataAPI(APIView):
 
     def post(self, request, *args, **kwargs):
+        logger.info(request.data)       
         record = Record(json_data=json.dumps(request.data))
         record.save()
         return Response({'message': 'ok'})
