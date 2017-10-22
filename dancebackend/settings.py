49fe7178bfef7c0141ade74d3a5baf2c3ea85bdd
liisa Sailaranta <liisa.sailaranta>
2017-10-22 10:37:23 -0700

    Add api endpoint and models to record a dance workout.

--- dancebackend/settings.py
+++ dancebackend/settings.py
@@ -10,14 +10,17 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 import os
+import environ
 
 from os.path import join, normpath
 
 import dj_database_url
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
